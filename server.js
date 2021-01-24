9428f915f91fc7dda0145f225bfd4bf33fb12021
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-01-24 09:22:31 +0200

    added get and post endpoints

--- server.js
+++ server.js
@@ -3,14 +3,32 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const server = express()
 const User = require('./models/User')
+const bodyParser = require('body-parser')
 
+// PARSE DATA TO JSON FORMAT
+server.use(bodyParser.json())
 
-
-server.get('/user', (req, res) => {
-    User.find({}, (req, data) =>
-        console.log(data)
+// GET ALL THE USERS
+server.get('/', (req, res) => {
+    const users = User.find({}, (req, user) => {
+        res.send(user)
+    }
     )
 })
+
+// ADD A NEW USER
+server.post('/add', (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        phone: req.body.phone
+    })
+    user.save()
+        .then(data => {
+            res.json(data)
+        })
+})
+
+
 // CONNECT TO DB
 mongoose.connect(
     process.env.DB_CONNECTION, {
@@ -18,4 +36,10 @@ mongoose.connect(
 },
     () => console.log('connected to DB!')
 )
-server.listen(3000)
+
+server.listen(3000, (err) => {
+    if (err) 
+        console.log(err)
+    else 
+        console.log('server connected on 3000')
+})
